a2dfc250a97b39a3ee9439c25d1525eadcd4fcd2
Sankalp Lakhina <sankalp.lakhina91@gmail.com>
2017-02-08 23:24:03 +0530

    Indentation and ES6 changes in webpack's dev and prod config

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,106 +1,103 @@
 import path from 'path';
+import autoprefixer from 'autoprefixer';
+
 import LodashModuleReplacementPlugin from 'lodash-webpack-plugin';
-const webpack = require('webpack');
 
-const assetsPath = path.resolve(__dirname, '../public/assets');
+import { webpackHost, webpackPort, reduxDevTools } from '../config/env';
+import webpack from 'webpack';
+import webpackIsomorphicToolsConfig from './webpack-isomorphic-tools';
+import WebpackIsomorphicToolsPlugin from 'webpack-isomorphic-tools/plugin';
 
-const { webpackHost, webpackPort, reduxDevTools } = require('../config/env');
-
-const webpackIsomorphicToolsConfig = require('./webpack-isomorphic-tools');
-const WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
-
-const webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(webpackIsomorphicToolsConfig);
-
-const autoprefixer = require('autoprefixer');
+const webpackIsomorphicTools = new WebpackIsomorphicToolsPlugin(webpackIsomorphicToolsConfig);
 
 module.exports = {
-  devtool: 'inline-source-map',
-  context: path.resolve(__dirname, '..'),
-  entry: {
-    main: [
-      `webpack-hot-middleware/client?path=http://${webpackHost}:${webpackPort}/__webpack_hmr`,
-      './src/client.js',
+    devtool: 'inline-source-map',
+    context: path.resolve(__dirname, '..'),
+    entry: {
+        main: [
+            `webpack-hot-middleware/client?path=http://${webpackHost}:${webpackPort}/__webpack_hmr`,
+            './src/client.js',
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, '../public/assets'),
+        filename: '[name]-[hash].js',
+        chunkFilename: '[name]-[chunkhash].js',
+        publicPath: `http://${webpackHost}:${webpackPort}/assets/`,
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    plugins: ['lodash'],
+                },
+            },
+            {
+                test: /\.less$/,
+                loader: 'style!css?importLoaders=2&sourceMap&localIdentName=[local]__[hash:base64:5]!less-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!postcss',
+            },
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff',
+            },
+            {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff',
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream',
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file',
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml',
+            },
+            {
+                test: webpackIsomorphicTools.regular_expression('images'),
+                loader: 'url-loader?limit=10240',
+            },
+        ],
+    },
+    postcss() {
+        return [autoprefixer];
+    },
+    progress: true,
+        resolve: {
+        modulesDirectories: [
+          '~/',
+          'node_modules',
+          'src',
+        ],
+        extensions: ['', '.json', '.js', '.jsx'],
+    },
+    plugins: [
+        // hot reload
+        new webpack.HotModuleReplacementPlugin(),
+        // for file created by WebpackIsomorphicToolsPlugin
+        new webpack.IgnorePlugin(/webpack-stats\.json$/),
+        // for optimized loading of lodash modules
+        new LodashModuleReplacementPlugin({
+            // collections: true,
+            // shorthands: true
+        }),
+        new webpack.DefinePlugin({
+            __CLIENT__: true,
+            __DEVTOOLS__: reduxDevTools,
+            'process.env': {
+                NODE_ENV: '"development"',
+            },
+        }),
+        webpackIsomorphicTools.development(),
     ],
-  },
-  output: {
-    path: assetsPath,
-    filename: '[name]-[hash].js',
-    chunkFilename: '[name]-[chunkhash].js',
-    publicPath: `http://${webpackHost}:${webpackPort}/assets/`,
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          plugins: ['lodash'],
-        },
-      },
-      {
-        test: /\.less$/,
-        loader: 'style!css?importLoaders=2&sourceMap&localIdentName=[local]__[hash:base64:5]!less-loader'
-      },
-      {
-        test: /\.css$/,
-        loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!postcss',
-      },
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff',
-      },
-      {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff',
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream',
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file',
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml',
-      },
-      {
-        test: webpackIsomorphicToolsPlugin.regular_expression('images'),
-        loader: 'url-loader?limit=10240',
-      },
-    ],
-  },
-  postcss() {
-    return [autoprefixer];
-  },
-  progress: true,
-  resolve: {
-    modulesDirectories: [
-      '~/',
-      'node_modules',
-      'src',
-    ],
-    extensions: ['', '.json', '.js', '.jsx'],
-  },
-  plugins: [
-    // hot reload
-    new webpack.HotModuleReplacementPlugin(),
-    // for file created by WebpackIsomorphicToolsPlugin
-    new webpack.IgnorePlugin(/webpack-stats\.json$/),
-    // for optimized loading of lodash modules
-    new LodashModuleReplacementPlugin({
-      // collections: true,
-      // shorthands: true
-    }),
-    new webpack.DefinePlugin({
-      __CLIENT__: true,
-      __DEVTOOLS__: reduxDevTools,
-      'process.env': {
-        NODE_ENV: '"development"',
-      },
-    }),
-    webpackIsomorphicToolsPlugin.development(),
-  ],
 };
--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,119 +1,116 @@
+require('./../server.babel'); // babel registration (runtime transpilation for node)
+
 const path = require('path');
+const autoprefixer = require('autoprefixer');
+
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const { webpackHost, webpackPort, reduxDevTools } = require('../config/env');
 const webpack = require('webpack');
-
-const assetsPath = path.resolve(__dirname, '../public/assets');
-
 const webpackIsomorphicToolsConfig = require('./webpack-isomorphic-tools');
 const WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
 
-const webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(webpackIsomorphicToolsConfig);
-
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const autoprefixer = require('autoprefixer');
+const webpackIsomorphicTools = new WebpackIsomorphicToolsPlugin(webpackIsomorphicToolsConfig);
 
 module.exports = {
-  devtool: 'source-map',
-  context: path.resolve(__dirname, '..'),
-  entry: {
-    main: [
-      './src/client.js',
-    ],
-  },
-  output: {
-    path: assetsPath,
-    filename: '[name]-[hash].js',
-    chunkFilename: '[name]-[chunkhash].js',
-    publicPath: '/assets/',
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          plugins: ['lodash'],
-        },
-      },
-      {
-        test: /\.less$/,
-        loader: ExtractTextPlugin.extract('style', 'css?importLoaders=2&sourceMap!less-loader')
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract(
-                  'style',
-                  'css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!postcss'
+    devtool: 'source-map',
+    context: path.resolve(__dirname, '..'),
+    entry: {
+        main: [
+            './src/client.js',
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, '../public/assets'),
+        filename: '[name]-[hash].js',
+        chunkFilename: '[name]-[chunkhash].js',
+        publicPath: '/assets/',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    plugins: ['lodash'],
+                },
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract('style', 'css?importLoaders=2&sourceMap!less-loader')
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract(
+                    'style',
+                    'css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!postcss'
                 ),
-      },
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff',
-      },
-      {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff',
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream',
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file',
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml',
-      },
-      {
-        test: webpackIsomorphicToolsPlugin.regular_expression('images'),
-        loader: 'url-loader?limit=10240',
-      },
+            },
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff',
+            },
+            {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff',
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream',
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file',
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml',
+            },
+            {
+                test: webpackIsomorphicTools.regular_expression('images'),
+                loader: 'url-loader?limit=10240',
+            },
+        ],
+    },
+    postcss() {
+        return [autoprefixer];
+    },
+    progress: true,
+    resolve: {
+        modulesDirectories: [
+            '~/',
+            'node_modules',
+            'src',
+        ],
+        extensions: ['', '.json', '.js', '.jsx'],
+    },
+    plugins: [
+        new ExtractTextPlugin('[name]-[chunkhash].css', { allChunks: true }),
+
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"',
+            },
+            __CLIENT__: true,
+            __DEVTOOLS__: false,
+        }),
+        new LodashModuleReplacementPlugin({
+            // collections: true,
+            // shorthands: true
+        }),
+
+        // ignore dev config
+        new webpack.IgnorePlugin(/\.\/dev/, /\/config$/),
+        // optimizations
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+        }),
+        webpackIsomorphicTools,
     ],
-  },
-  postcss() {
-    return [autoprefixer];
-  },
-  progress: true,
-  resolve: {
-    modulesDirectories: [
-      '~/',
-      'node_modules',
-      'src',
-    ],
-    extensions: ['', '.json', '.js', '.jsx'],
-  },
-  plugins: [
-    new ExtractTextPlugin('[name]-[chunkhash].css', { allChunks: true }),
-
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"',
-      },
-
-      __CLIENT__: true,
-      __DEVTOOLS__: false,
-    }),
-
-    new LodashModuleReplacementPlugin({
-      // collections: true,
-      // shorthands: true
-    }),
-
-    // ignore dev config
-    new webpack.IgnorePlugin(/\.\/dev/, /\/config$/),
-
-    // optimizations
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-    }),
-    webpackIsomorphicToolsPlugin,
-  ],
 };
